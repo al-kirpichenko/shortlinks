6a332cc97ab39c0c57adc6d2dde6ff24a448d3ff
al-kirpichenko <fuluganchik@yandex.ru>
2023-05-05 21:08:01 +0700

    инкремент 5, удаляем пробелы из переменных окружения

--- config/config.go
+++ config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"flag"
 	"os"
+	"strings"
 )
 
 var AppConfig struct {
@@ -15,12 +16,12 @@ var AppConfig struct {
 func init() {
 
 	if host := os.Getenv("SERVER_ADDRESS"); host != "" {
-		AppConfig.Host = host
+		AppConfig.Host = strings.TrimSpace(host)
 	} else {
 		flag.StringVar(&AppConfig.Host, "a", "localhost:8080", "It's a Host")
 	}
 	if baseURL := os.Getenv("BASE_URL"); baseURL != "" {
-		AppConfig.ResultURL = baseURL
+		AppConfig.ResultURL = strings.TrimSpace(baseURL)
 	} else {
 		flag.StringVar(&AppConfig.ResultURL, "b", "http://localhost:8080", "It's a Result URL")
 	}
