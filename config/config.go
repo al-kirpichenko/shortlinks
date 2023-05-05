2846d190e15798a26a17fb826cf482c443e02ad3
al-kirpichenko <fuluganchik@yandex.ru>
2023-05-05 21:23:32 +0700

    инкремент 5, удалено лишнее ветвление

--- config/config.go
+++ config/config.go
@@ -17,14 +17,12 @@ func init() {
 
 	if host := os.Getenv("SERVER_ADDRESS"); host != "" {
 		AppConfig.Host = strings.TrimSpace(host)
-	} else {
-		flag.StringVar(&AppConfig.Host, "a", "localhost:8080", "It's a Host")
 	}
+	flag.StringVar(&AppConfig.Host, "a", "localhost:8080", "It's a Host")
 	if baseURL := os.Getenv("BASE_URL"); baseURL != "" {
 		AppConfig.ResultURL = strings.TrimSpace(baseURL)
-	} else {
-		flag.StringVar(&AppConfig.ResultURL, "b", "http://localhost:8080", "It's a Result URL")
 	}
+	flag.StringVar(&AppConfig.ResultURL, "b", "http://localhost:8080", "It's a Result URL")
 
 	//flag.StringVar(&AppConfig.Host, "a", "localhost:8080", "It's a Host")
 	//flag.StringVar(&AppConfig.ResultURL, "b", "http://localhost:8080", "It's a Result URL")
