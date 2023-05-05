bf183f154f5c25fad8116f317e924a6fc0657050
al-kirpichenko <fuluganchik@yandex.ru>
2023-05-05 19:50:31 +0700

    инкремент 4, создан пакет config, добавлены аргументы командной строки

--- config/config.go
+++ config/config.go
@@ -9,9 +9,6 @@ var AppConfig struct {
 
 func init() {
 
-	//AppConfig.Host = "localhost:8080"
-	//AppConfig.ResultURL = "http://localhost:8080"
-
 	flag.StringVar(&AppConfig.Host, "a", "localhost:8080", "It's a Host")
 	flag.StringVar(&AppConfig.ResultURL, "b", "http://localhost:8080", "It's a Result URL")
 
