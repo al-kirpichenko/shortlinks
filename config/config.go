0111b4b36b125c301cabc38e09d7c56033a2b852
al-kirpichenko <fuluganchik@yandex.ru>
2023-05-05 20:59:34 +0700

    инкремент 5, добавлены переменные окружения

--- config/config.go
+++ config/config.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"flag"
-	"os"
 )
 
 var AppConfig struct {
@@ -14,15 +13,18 @@ var AppConfig struct {
 
 func init() {
 
-	if host := os.Getenv("SERVER_ADDRESS"); host != "" {
-		AppConfig.Host = host
-	} else {
-		flag.StringVar(&AppConfig.Host, "a", "localhost:8080", "It's a Host")
-	}
-	if baseURL := os.Getenv("BASE_URL"); baseURL != "" {
-		AppConfig.ResultURL = baseURL
-	} else {
-		flag.StringVar(&AppConfig.ResultURL, "b", "http://localhost:8080", "It's a Result URL")
-	}
+	//if host := os.Getenv("SERVER_ADDRESS"); host != "" {
+	//	AppConfig.Host = host
+	//} else {
+	//	flag.StringVar(&AppConfig.Host, "a", "localhost:8080", "It's a Host")
+	//}
+	//if baseURL := os.Getenv("BASE_URL"); baseURL != "" {
+	//	AppConfig.ResultURL = baseURL
+	//} else {
+	//	flag.StringVar(&AppConfig.ResultURL, "b", "http://localhost:8080", "It's a Result URL")
+	//}
+
+	flag.StringVar(&AppConfig.Host, "a", "localhost:8080", "It's a Host")
+	flag.StringVar(&AppConfig.ResultURL, "b", "http://localhost:8080", "It's a Result URL")
 
 }
