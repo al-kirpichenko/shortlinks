a06dc2206b1eb573f7e1be9b6bd41b94ae421a7d
al-kirpichenko <fuluganchik@yandex.ru>
2023-05-06 09:48:21 +0700

    инкремент 5: - конфигурация через env должна быть приоритетной и переписывать значение флага - исправление

--- config/config.go
+++ config/config.go
@@ -15,12 +15,13 @@ var AppConfig struct {
 
 func init() {
 
-	if host := os.Getenv("SERVER_ADDRESS"); host != "" {
-		AppConfig.Host = strings.TrimSpace(host)
-	}
 	flag.StringVar(&AppConfig.Host, "a", "localhost:8080", "It's a Host")
+	flag.StringVar(&AppConfig.ResultURL, "b", "http://localhost:8080", "It's a Result URL")
+
 	if baseURL := os.Getenv("BASE_URL"); baseURL != "" {
 		AppConfig.ResultURL = strings.TrimSpace(baseURL)
 	}
-	flag.StringVar(&AppConfig.ResultURL, "b", "http://localhost:8080", "It's a Result URL")
+	if host := os.Getenv("SERVER_ADDRESS"); host != "" {
+		AppConfig.Host = strings.TrimSpace(host)
+	}
 }
