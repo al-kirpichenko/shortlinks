e3f2066ab685df99a4db5f2123f47483f35b6bde
al-kirpichenko <fuluganchik@yandex.ru>
2023-05-20 11:02:41 +0700

    Инктемент 9 - ревью - добавлен пакет env для работы с переменными окружения

--- cmd/shortener/config/config.go
+++ cmd/shortener/config/config.go
@@ -2,8 +2,8 @@ package config
 
 import (
 	"flag"
-	"os"
-	"strings"
+	"github.com/caarlos0/env/v6"
+	"log"
 )
 
 type AppConfig struct {
@@ -12,23 +12,35 @@ type AppConfig struct {
 	FilePATH  string
 }
 
+type Config struct {
+	Host      string `env:"SERVER_ADDRESS"`
+	ResultURL string `env:"BASE_URL"`
+	FilePATH  string `env:"FILE_STORAGE_PATH"`
+}
+
 func NewCfg() *AppConfig {
 
 	a := &AppConfig{}
 
 	flag.StringVar(&a.Host, "a", "localhost:8080", "It's a Host")
 	flag.StringVar(&a.ResultURL, "b", "http://localhost:8080", "It's a Result URL")
-	//flag.StringVar(&a.FilePATH, "f", "/tmp/short-url-db.json", "It's a FilePATH")
-	flag.StringVar(&a.FilePATH, "f", "E://GoProjects/shortlinks/tmp/short-url-db.json", "It's a FilePATH")
+	flag.StringVar(&a.FilePATH, "f", "/tmp/short-url-db.json", "It's a FilePATH")
+	//flag.StringVar(&a.FilePATH, "f", "E://GoProjects/shortlinks/tmp/short-url-db.json", "It's a FilePATH")
 
-	if baseURL := os.Getenv("BASE_URL"); baseURL != "" {
-		a.ResultURL = strings.TrimSpace(baseURL)
+	err := env.Parse(a)
+	if err != nil {
+		log.Fatal(err)
 	}
-	if host := os.Getenv("SERVER_ADDRESS"); host != "" {
-		a.Host = strings.TrimSpace(host)
-	}
-	if file := os.Getenv("FILE_STORAGE_PATH"); file != "" {
-		a.FilePATH = file
-	}
+
+	//if baseURL := os.Getenv("BASE_URL"); baseURL != "" {
+	//	a.ResultURL = strings.TrimSpace(baseURL)
+	//}
+	//if host := os.Getenv("SERVER_ADDRESS"); host != "" {
+	//	a.Host = strings.TrimSpace(host)
+	//}
+	//if file := os.Getenv("FILE_STORAGE_PATH"); file != "" {
+	//	a.FilePATH = file
+	//}
+
 	return a
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module github.com/al-kirpichenko/shortlinks
 go 1.20
 
 require (
+	github.com/caarlos0/env/v6 v6.10.1
 	github.com/go-chi/chi/v5 v5.0.8
 	github.com/stretchr/testify v1.8.2
+	go.uber.org/zap v1.24.0
 )
 
 require (
@@ -12,6 +14,5 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	go.uber.org/zap v1.24.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
