8df69a2f9d881135ae68a1bae74c3e34fc618994
al-kirpichenko <fuluganchik@yandex.ru>
2023-05-25 22:46:43 +0700

    инкремент 9 - env

--- cmd/shortener/config/config.go
+++ cmd/shortener/config/config.go
@@ -2,33 +2,39 @@ package config
 
 import (
 	"flag"
-	"os"
-	"strings"
+	//"os"
+	//"strings"
+	"github.com/caarlos0/env"
 )
 
 type AppConfig struct {
-	Host      string
-	ResultURL string
-	FilePATH  string
+	Host      string `env:"SERVER_ADDRESS"`
+	ResultURL string `env:"BASE_URL"`
+	FilePATH  string `env:"FILE_STORAGE_PATH"`
 }
 
 func NewAppConfig() *AppConfig {
 
-	a := &AppConfig{}
+	a := AppConfig{}
 
 	flag.StringVar(&a.Host, "a", "localhost:8080", "It's a Host")
 	flag.StringVar(&a.ResultURL, "b", "http://localhost:8080", "It's a Result URL")
 	flag.StringVar(&a.FilePATH, "f", "/tmp/short-url-db.json", "It's a FilePATH")
 	flag.Parse()
 
-	if baseURL := os.Getenv("BASE_URL"); baseURL != "" {
-		a.ResultURL = strings.TrimSpace(baseURL)
+	err := env.Parse(&a)
+	if err != nil {
+		panic(err)
 	}
-	if host := os.Getenv("SERVER_ADDRESS"); host != "" {
-		a.Host = strings.TrimSpace(host)
-	}
-	if file := os.Getenv("FILE_STORAGE_PATH"); file != "" {
-		a.FilePATH = file
-	}
-	return a
+	//if baseURL := os.Getenv("BASE_URL"); baseURL != "" {
+	//	a.ResultURL = strings.TrimSpace(baseURL)
+	//}
+	//if host := os.Getenv("SERVER_ADDRESS"); host != "" {
+	//	a.Host = strings.TrimSpace(host)
+	//}
+	//if file := os.Getenv("FILE_STORAGE_PATH"); file != "" {
+	//	a.FilePATH = file
+	//}
+
+	return &a
 }
