a84ed4a0735caade952fb4ec134baca4da026145
al-kirpichenko <fuluganchik@yandex.ru>
2023-08-24 12:56:48 +0700

    инкремент 16

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -4,9 +4,10 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	middle "github.com/go-chi/chi/v5/middleware"
 
 	"github.com/al-kirpichenko/shortlinks/internal/app"
+	"github.com/al-kirpichenko/shortlinks/internal/middleware"
 	"github.com/al-kirpichenko/shortlinks/internal/middleware/cookies"
 	"github.com/al-kirpichenko/shortlinks/internal/middleware/logger"
 )
@@ -16,7 +17,7 @@ func Router(app *app.App) http.Handler {
 	router := chi.NewRouter()
 
 	router.Use(logger.Logger)
-	//router.Use(middleware.Gzip)
+	router.Use(middleware.Gzip)
 	router.Use(cookies.Cookies)
 
 	router.Get("/{id}", app.GetOriginalURL)
@@ -26,7 +27,7 @@ func Router(app *app.App) http.Handler {
 	router.Post("/api/shorten/batch", app.APIBatch)
 	router.Get("/api/user/urls", app.APIGetUserURLs)
 	router.Delete("/api/user/urls", app.APIDelUserURLs)
-	router.Mount("/debug", middleware.Profiler())
+	router.Mount("/debug", middle.Profiler())
 
 	return router
 
