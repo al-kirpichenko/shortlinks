304c0747d66b12b0b51621966f9f9eba4ac11a02
al-kirpichenko <fuluganchik@yandex.ru>
2023-08-31 18:38:03 +0700

    инкремент 18 go fml ci

--- cmd/shortener/config/config.go
+++ cmd/shortener/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"flag"
+	"fmt"
 
 	"github.com/caarlos0/env"
 )
@@ -13,14 +14,24 @@ type AppConfig struct {
 	DataBaseString string `env:"DATABASE_DSN"`
 }
 
+const (
+	DBhost     = "localhost"
+	DBuser     = "postgres"
+	DBpassword = "123"
+	DBdbname   = "postgres"
+)
+
 func NewAppConfig() *AppConfig {
 
 	a := AppConfig{}
+	ps := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable",
+		DBhost, DBuser, DBpassword, DBdbname)
 
 	flag.StringVar(&a.Host, "a", "localhost:8080", "It's a Host")
 	flag.StringVar(&a.ResultURL, "b", "http://localhost:8080", "It's a Result URL")
 	flag.StringVar(&a.FilePATH, "f", "", "It's a FilePATH")
-	flag.StringVar(&a.DataBaseString, "d", "", "it's conn string")
+	//flag.StringVar(&a.DataBaseString, "d", "", "it's conn string")
+	flag.StringVar(&a.DataBaseString, "d", ps, "it's conn string")
 
 	flag.Parse()
 
