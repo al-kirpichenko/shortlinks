03a1deb2f4efa83cb501b640356f145b27cdb695
al-kirpichenko <fuluganchik@yandex.ru>
2023-10-10 15:35:25 +0700

    инкремент 19 - рефакторинг

---
+++ cmd/staticlint/doc.go
@@ -0,0 +1,5 @@
+// Модуль staticlint содержит линтеры:
+// стандартные статические анализаторы пакета golang.org/x/tools/go/analysis/passes
+// все анализаторы класса SA пакета staticcheck.io
+// анализатор, запрещающий использовать прямой вызов os.Exit в функции main пакета main
+package main
--- cmd/staticlint/multichecker.go
+++ cmd/staticlint/multichecker.go
@@ -1,8 +1,3 @@
-// Модуль staticlint содержит линтеры:
-// стандартные статические анализаторы пакета golang.org/x/tools/go/analysis/passes
-// все анализаторы класса SA пакета staticcheck.io
-// анализатор, запрещающий использовать прямой вызов os.Exit в функции main пакета main
-
 package main
 
 import (
