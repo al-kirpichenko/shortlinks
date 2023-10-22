a37bdb313f39ad93416fbb7da21e2fd601c7e6cb
al-kirpichenko <fuluganchik@yandex.ru>
2023-10-22 18:20:15 +0700

    Инкремент 22 - graceful shutdown

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -5,6 +5,11 @@ import (
 	"log"
 	"net/http"
 	_ "net/http/pprof"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"golang.org/x/net/context"
 
 	"github.com/al-kirpichenko/shortlinks/cmd/shortener/config"
 	"github.com/al-kirpichenko/shortlinks/internal/app"
@@ -32,13 +37,66 @@ func main() {
 
 	newApp.ConfigureStorage()
 
-	router := routes.Router(newApp)
+	srv := &http.Server{
+		Addr:    conf.Host,
+		Handler: routes.Router(newApp),
+	}
 
-	defer newApp.DB.Close()
+	// через этот канал сообщим основному потоку, что соединения закрыты
+	idleConnsClosed := make(chan struct{})
 
+	sigint := make(chan os.Signal, 1)
+	// регистрируем перенаправление прерываний
+	signal.Notify(sigint, os.Interrupt, syscall.SIGQUIT)
+
+	// запускаем горутину обработки пойманных прерываний
+	go func() {
+		// читаем из канала прерываний
+		// поскольку нужно прочитать только одно прерывание,
+		// можно обойтись без цикла
+		<-sigint
+		// получили сигнал os.Interrupt, запускаем процедуру graceful shutdown
+		if err := srv.Shutdown(context.Background()); err != nil {
+			// ошибки закрытия Listener
+			log.Printf("HTTP server Shutdown: %v", err)
+		}
+		// сообщаем основному потоку,
+		// что все сетевые соединения обработаны и закрыты
+		close(idleConnsClosed)
+	}()
 	if conf.EnableHTTPS {
-		log.Fatal(http.ListenAndServeTLS(conf.Host, "./certs/cert.pem", "./certs/key.pem", router))
+		runHTTPS(srv)
 	} else {
-		log.Fatal(http.ListenAndServe(conf.Host, router))
+		run(srv)
+	}
+
+	// ждём завершения процедуры graceful shutdown
+	<-idleConnsClosed
+	// получили оповещение о завершении
+	// здесь можно освобождать ресурсы перед выходом,
+	// например закрыть соединение с базой данных,
+	// закрыть открытые файлы
+	fmt.Println("Server Shutdown gracefully")
+
+	//if conf.EnableHTTPS {
+	//	log.Fatal(http.ListenAndServeTLS(conf.Host, "./certs/cert.pem", "./certs/key.pem", router))
+	//} else {
+	//	log.Fatal(http.ListenAndServe(conf.Host, router))
+	//}
+}
+
+func run(srv *http.Server) {
+
+	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+		// ошибки запуска или остановки Listener
+		log.Fatalf("HTTP server ListenAndServe: %v", err)
+	}
+}
+
+func runHTTPS(srv *http.Server) {
+
+	if err := srv.ListenAndServeTLS("./certs/cert.pem", "./certs/key.pem"); err != http.ErrServerClosed {
+		// ошибки запуска или остановки Listener
+		log.Fatalf("HTTP server ListenAndServe: %v", err)
 	}
 }
