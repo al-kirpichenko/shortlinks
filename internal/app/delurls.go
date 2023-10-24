6f7caf2853ec99b42013642ac381641cd80d29b9
al-kirpichenko <fuluganchik@yandex.ru>
2023-10-25 01:34:46 +0700

    Инкремент 23 - поиск ошибки автотеста

--- internal/app/delurls.go
+++ internal/app/delurls.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/al-kirpichenko/shortlinks/internal/middleware/cookies"
@@ -10,6 +11,10 @@ import (
 )
 
 func (a *App) APIDelUserURLs(w http.ResponseWriter, r *http.Request) {
+
+	log.Println("TEST DELETE API")
+
+	w.WriteHeader(http.StatusAccepted)
 
 	var shorts []string
 
@@ -33,5 +38,4 @@ func (a *App) APIDelUserURLs(w http.ResponseWriter, r *http.Request) {
 		URLs:   shorts,
 	})
 
-	w.WriteHeader(http.StatusAccepted)
 }
