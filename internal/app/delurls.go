8e78783541105b212f7104c00ade37c4397de88d
al-kirpichenko <fuluganchik@yandex.ru>
2023-10-25 03:21:09 +0700

    Инкремент 23 - поиск ошибки автотеста

--- internal/app/delurls.go
+++ internal/app/delurls.go
@@ -1,36 +1,41 @@
 package app
 
 import (
+	"encoding/json"
 	"net/http"
+
+	"github.com/al-kirpichenko/shortlinks/internal/middleware/cookies"
+	"github.com/al-kirpichenko/shortlinks/internal/services/delurls"
+	"github.com/al-kirpichenko/shortlinks/internal/services/userid"
 )
 
 func (a *App) APIDelUserURLs(w http.ResponseWriter, r *http.Request) {
 
+	var shorts []string
+
+	token := r.Context().Value(cookies.ContextUserKey).(string)
+
+	userID, err := userid.GetUserID(token)
+	if err != nil {
+		http.Error(w, "user id not found in cookie", http.StatusUnauthorized)
+		return
+	}
+
 	w.WriteHeader(http.StatusAccepted)
 
-	//var shorts []string
-	//
-	//token := r.Context().Value(cookies.ContextUserKey).(string)
-	//
-	//userID, err := userid.GetUserID(token)
-	//if err != nil {
-	//	http.Error(w, "user id not found in cookie", http.StatusUnauthorized)
-	//	return
-	//}
-	//
-	//w.WriteHeader(http.StatusAccepted)
-	//
-	//err = json.NewDecoder(r.Body).Decode(&shorts)
-	//if err != nil {
-	//	http.Error(w, err.Error(), http.StatusBadRequest)
-	//	return
-	//}
-	//
-	//queue := delurls.NewQueue(a.Channel)
-	//
-	//queue.Push(&delurls.Task{
-	//	UserID: userID,
-	//	URLs:   shorts,
-	//})
+	err = json.NewDecoder(r.Body).Decode(&shorts)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	queue := delurls.NewQueue(a.Channel)
+
+	queue.Push(&delurls.Task{
+		UserID: userID,
+		URLs:   shorts,
+	})
+
+	return
 
 }
