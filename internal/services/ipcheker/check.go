74041e1056a31d9d84e351a6914931a02b9402d5
al-kirpichenko <fuluganchik@yandex.ru>
2023-12-01 12:03:11 +0700

    инкр. 25 исправлена проверка ip

--- internal/services/ipcheker/check.go
+++ internal/services/ipcheker/check.go
@@ -3,12 +3,11 @@ package ipcheker
 import (
 	"net"
 	"net/http"
-	"strings"
 )
 
 // CheckIP -
 func CheckIP(r *http.Request, trustedSubnet string) bool {
-	ip := strings.Split(r.Header.Get("X-Real-IP"), ":")[0]
+	ip := r.Header.Get("X-Real-IP")
 	_, ipNet, err := net.ParseCIDR(trustedSubnet)
 	if err != nil {
 		return false
